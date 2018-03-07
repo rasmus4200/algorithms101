c6a542b4d83b1056d3fe9509e6db91962f735696
Jonathan Rasmusson <jonathan_rasmusson@transcanada.com>
2018-03-07 11:07:19 -0700

    Add Ransom note

--- src/test/java/questions/classics/ClassicsTest.java
+++ src/test/java/questions/classics/ClassicsTest.java
@@ -11,10 +11,12 @@ import java.util.ArrayList;
 public class ClassicsTest {
 
     private FizzBuzz fizzBuzz;
+    private RansomNote ransomNote;
 
     @Before
     public void SetUp() {
         fizzBuzz = new FizzBuzz();
+        ransomNote = new RansomNote();
     }
 
     @Test
@@ -25,6 +27,23 @@ public class ClassicsTest {
         // For numbers which are multiples of both three and five print 'FizzBuzz'.
 
         fizzBuzz.print();
+    }
+
+    @Test
+    public void HarmlessRansomNote() {
+        // Challenge: Write a ransom note by cutting letters out of the available
+        // magazines and pasting them together to form a letter. Given an arbitrary
+        // ransom note string and another string containing all the contents of all
+        // the magazines, write a function that will return true if the ransom
+        // note can be made from the magazines; otherwise, it will return false.
+        // Every letter found in the magazine string can only be used once in your
+        // ransom note.
+
+        Assert.assertTrue(ransomNote.canWrite("Pay", "yaP"));
+        Assert.assertTrue(ransomNote.canWrite("Pay", "yaP a"));
+        Assert.assertTrue(ransomNote.canWrite("Pay me $1000", "ayPem0001$"));
+
+        Assert.assertFalse(ransomNote.canWrite("Pay", "Pa"));
     }
 
 }
