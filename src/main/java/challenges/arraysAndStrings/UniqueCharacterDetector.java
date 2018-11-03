52b784a2bc78ce68c702660fab18f09284be6227
jrasmusson <rasmus4200@gmail.com>
2018-11-03 07:51:38 -0600

    Add comment for uniqueChar question

--- src/main/java/challenges/arraysAndStrings/UniqueCharacterDetector.java
+++ src/main/java/challenges/arraysAndStrings/UniqueCharacterDetector.java
@@ -20,7 +20,7 @@ public class UniqueCharacterDetector {
 
         boolean[] char_set = new boolean[128];
         for (int i = 0; i < text.length(); i++) {
-            int val = text.charAt(i);
+            int val = text.charAt(i); // char can be int
             if (char_set[val]) { // Already found
                 return false;
             }
