58a39093f0216a51df47bb9bcc0da2cb5ecbf75e
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2019-12-14 15:06:18 +0100

    Remove implementation of Array to make tests fail. Make DynamicArray generic.

--- src/main/java/datastructures/DynamicArray.java
+++ src/main/java/datastructures/DynamicArray.java
@@ -1,6 +1,6 @@
 package datastructures;
 
-public class DynamicArray<String> {
+public class DynamicArray<T> {
 
     private Object[] data;
 
@@ -16,104 +16,41 @@ public class DynamicArray<String> {
         data = new Object[initialCapacity];
     }
 
-    public String get(int index) {
-        return (String)data[index];
+    public T get(int index) {
+        return null;
     }
 
-    public void set(int index, String value) {
-        data[index] = value;
+    public void set(int index, T value) {
     }
 
-    public void insert(int index, String value) {
-
-        // Check size
-        if (size == initialCapacity) {
-            resize();
-        }
-
-        // Copy up
-        for (int j = size; j > index; j--) {
-            data[j] = data[j-1];
-        }
-
-        // Insert
-        data[index] = value;
-        size++;
+    public void insert(int index, T value) {
     }
 
-    //
-    // You try
-    // Try implementing delete, isEmpty, and Contains on your own.
-    // You can find the solution at the bottom
-
-    /*
     public void delete(int index) {
     }
 
     public boolean isEmpty() {
-
+        return false;
     }
 
-    public boolean Contains(String value) {
+    public boolean Contains(T value) {
+        return false;
     }
-    */
 
-    private void resize() {
-        Object[] newData = new Object[initialCapacity * 2];
-        for (int i = 0; i < initialCapacity; i++) {
-            newData[i] = data[i];
-        }
-        data = newData;
-        initialCapacity = initialCapacity * 2;
-    }
 
     public int size() {
-        return size;
+        return 0;
     }
 
     public void print() {
-        for (int i=0;i< size;i++) {
-            System.out.println("data[i] = " + data[i]);
-        }
+
     }
 
     // Some array implementations offer methods like insert (also known as pushBack).
     // Which is like insert, only in does the insert at the end of the array.
-    public void add(String value) {
-
-        if (size == initialCapacity) {
-            resize();
-        }
-
-        data[size] = value;
-        size++;
+    public void add(T value) {
     }
 
-    public void delete(int index) {
-        // Copy down
-        for (int j = index; j < size - 1; j++) {
-            data[j] = data[j + 1];
-        }
-        size--;
-
-        // Clear if last element in array
-        if (index == size) {
-            data[index] = null;
-        }
+    private void resize() {
     }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public boolean Contains(String value) {
-        for (int i = 0; i <= size -  1; i++) {
-            String currentValue = (String)data[i];
-            if (currentValue.equals(value)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
