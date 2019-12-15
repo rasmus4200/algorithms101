01b5a950f14f9ac4a7a069a3a8a019ea41134f1c
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2019-12-15 19:24:19 +0100

    Remove implementation of LinkedList to make tests fail. Make LinkedList generic.

--- src/main/java/datastructures/LinkedList.java
+++ src/main/java/datastructures/LinkedList.java
@@ -1,128 +1,51 @@
 package datastructures;
 
-public class LinkedList {
+public class LinkedList<E> {
 
-    public class Node {
-        int data;
+    public class Node<E> {
+        E data;
         Node next;
 
-        public Node(int data) {
+        public Node(E data) {
             this.data = data;
         }
     }
 
     public Node head;
 
-    public void addFront(int data) {
+    public void addFront(E data) {
 
-        // Create new node
-        Node newNode = new Node(data);
 
-        // if head...
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        // Set it's next to current head
-        newNode.next = head;
-
-        // Set current head equal to this new head
-        head = newNode;
     }
 
-    public int getFirst() {
-        return head.data;
+    public E getFirst() {
+        return null;
     }
 
-    public int getLast() {
-        if (head == null) {
-            throw new IllegalStateException("Empty list!");
-        }
-
-        Node current = head;
-
-        // while we are not at the tail
-        while (current.next != null) {
-            current = current.next; // O(n)
-        }
-
-        // We are at the tail
-        return current.data;
+    public E getLast() {
+        return null;
     }
 
-    public void addBack(int data) {
-        Node newNode = new Node(data);
+    public void addBack(E data) {
 
-        // if head... set and return
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        // Else starting at head...
-        Node current = head;
-
-        // Walk until to hit tail
-        while (current.next != null) {
-            current = current.next;
-        }
-
-        // Set current node to equal newNode
-        current.next = newNode;
     }
 
     public int size() {
 
-        if (head == null) {
-            return 0;
-        }
-
-        int count = 1;
-        Node current = head;
-
-        while (current.next != null) {
-            current = current.next;
-            count++;
-        }
-
-        return count;
+        return 0;
     }
 
     public void clear() {
-        head = null;
+
     }
 
-    public void deleteValue(int data) {
+    public void deleteValue(E data) {
 
-        // if head
-        if (head == null) {
-            return;
-        }
-        if (head.data == data) {
-            head = head.next;
-            return;
-        }
 
-        // else walk the list
-        Node current = head;
-
-        while (current.next != null) {
-            if (current.next.data == data) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
-        }
     }
 
     public void print() {
-        Node current = head;
-        while (current != null) {
-            System.out.println(current.data);
-            current = current.next;
-        }
-        System.out.println("");
+
     }
 
 }
