fc7effbe720f05229dba66d0d49362abd0ab0029
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2019-12-16 19:55:20 +0100

    Remove implementation of HashTable to make tests fail. Make HashTable generic.

--- src/main/java/datastructures/HashTable.java
+++ src/main/java/datastructures/HashTable.java
@@ -1,16 +1,16 @@
 package datastructures;
 
-public class HashTable {
+public class HashTable<K, V> {
 
     private int INITIAL_SIZE = 16;
     private HashEntry[] data; // LinkedList
 
-    class HashEntry {
-        String key;
-        String value;
+    class HashEntry<K, V> {
+        K key;
+        V value;
         HashEntry next;
 
-        HashEntry(String key, String value) {
+        HashEntry(K key, V value) {
             this.key = key;
             this.value = value;
             this.next = null;
@@ -21,91 +21,36 @@ public class HashTable {
         data = new HashEntry[INITIAL_SIZE];
     }
 
-    public void put(String key, String value) {
+    public void put(K key, V value) {
 
-        // Get the index
-        int index = getIndex(key);
-
-        // Create the linked list entry
-        HashEntry entry = new HashEntry(key, value);
-
-        // If no entry there - add it
-        if (data[index] == null) {
-            data[index] = entry;
-        }
-        // Else handle collision by adding to end of linked list
-        else {
-            HashEntry entries = data[index];
-            // Walk to the end...
-            while(entries.next != null) {
-                entries = entries.next;
-            }
-            // Add our new entry there
-            entries.next = entry;
-        }
     }
 
-    public String get(String key) {
+    public V get(String key) {
 
-        // Get the index
-        int index = getIndex(key);
-
-        // Get the current list of entries
-        HashEntry entries = data[index];
-
-        // if we have existing entries against this key...
-        if (entries != null) {
-            // else walk chain until find a match
-            while (!entries.key.equals(key) && entries.next !=null) {
-                entries = entries.next;
-            }
-            // then return it
-            return entries.value;
-        }
-
-        // it we have no entries against this key...
-       return null;
+        return null;
     }
 
-    private int getIndex(String key) {
-        // Get the hash code
-        int hashCode = key.hashCode();
-        System.out.println("hashCode = " + hashCode);
+    private int getIndex(K key) {
 
-        // Convert to index
-        int index = (hashCode & 0x7fffffff) % INITIAL_SIZE;
-//        int index = hashCode % INITIAL_SIZE;
+        int index = 0;
 
         // Hack to force collision for testing
+        /*
         if (key.equals("John Smith") || key.equals("Sandra Dee")) {
             index = 4;
         }
-
-        System.out.println("index = " + index);
+        */
 
         return index;
     }
 
     @Override
     public String toString() {
-        int bucket = 0;
-        StringBuilder hashTableStr = new StringBuilder();
-        for (HashEntry entry : data) {
-            if(entry == null) {
-                continue;
-            }
-            hashTableStr.append("\n bucket[")
-                    .append(bucket)
-                    .append("] = ")
-                    .append(entry.toString());
-            bucket++;
-            HashEntry temp = entry.next;
-            while(temp != null) {
-                hashTableStr.append(" -> ");
-                hashTableStr.append(temp.toString());
-                temp = temp.next;
-            }
-        }
-        return hashTableStr.toString();
+
+        /**
+         * TODO: Try to use a StringBuilder here
+         */
+
+        return null;
     }
 }
