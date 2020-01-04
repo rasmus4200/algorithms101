b2c972b7207db4267b3aa0d86258dfbadd228f19
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2020-01-04 22:30:08 +0100

    Remove sort algorithm implementation

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -4,6 +4,16 @@ public class BubbleSort {
 
     public int[] sort(int arr[])
     {
+        for(int i=arr.length-1; i>0; i--) {
+            for(int j=0; j<i; j++) {
+                System.out.println(j);
+                if(arr[j]>arr[j+1]) {
+                    int buffer = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = buffer;
+                }
+            }
+        }
         return arr;
     }
 }
