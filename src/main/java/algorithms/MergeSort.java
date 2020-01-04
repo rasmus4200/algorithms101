4b4da65aefc5a2f9bc081a3962876a89f23ae2e3
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2020-01-04 22:16:42 +0100

    Remove sort algorithm implementation

--- src/main/java/algorithms/MergeSort.java
+++ src/main/java/algorithms/MergeSort.java
@@ -6,19 +6,7 @@ public class MergeSort {
     // merge()
     public void sort(int arr[], int l, int r)
     {
-        System.out.println("splitting l r: " + l + " " + r);
-        if (l < r)
-        {
-            // Find the middle point
-            int m = (l+r)/2;
 
-            // Sort first and second halves
-            sort(arr, l, m);
-            sort(arr , m+1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
-        }
     }
 
     // Merges two subarrays of arr[].
@@ -26,63 +14,7 @@ public class MergeSort {
     // Second subarray is arr[m+1..r]
     public void merge(int arr[], int l, int m, int r)
     {
-        System.out.println("merge l m r: " + l + " " + m + " " + r);
 
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
-
-        /* Copy data to temp arrays */
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
-
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarry array
-        int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
-                arr[k] = L[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-
-        System.out.println("After merge");
-        printArray(arr);
     }
 
     static void printArray(int arr[])
--- src/main/java/algorithms/QuickSort.java
+++ src/main/java/algorithms/QuickSort.java
@@ -12,52 +12,16 @@ public class QuickSort {
         // Then we repeat the process to the left and right portions over and over again
         // Then eventually our array becomes sorted.
 
-        quickSort(array, 0, array.length - 1);
 
         return array;
     }
 
     private void quickSort(int[] array, int left, int right) {
-        if (left >= right) {
-            return;
-        }
 
-        // Step 1: Pick a pivot element - we will choose the center
-        // Better would be to choose left + (right-left)/2 (as this would avoid overflow error for large arrays i.e. 2GB))
-        int pivot = array[(left + right)/2];
-
-        // Step 2: Partition the array around this pivot - return the index of the partition
-        int index = partition(array, left, right, pivot);
-
-        // Step 3: Sort on the left and the right side
-        quickSort(array, left, index - 1);
-        quickSort(array, index, right);
     }
 
     private int partition(int[] array, int left, int right, int pivot) {
-        // Move the left and right pointers in towards each other
-        while (left <= right) {
 
-            // Move left until you find an element bigger than the pivot
-            while(array[left] < pivot) {
-                left++;
-            }
-
-            // Move right until you find an element smaller than the pivot
-            while (array[right] > pivot) {
-                right--;
-            }
-
-            // Then swap
-            if (left <= right) {
-                swap(array, left, right);
-                left++;
-                right--;
-            }
-        }
-
-        // When we get here, everything in this partition will be in the right order
-        // Now we need to return next partition point - which for us will be left
         return left;
     }
 
