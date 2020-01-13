a0954169e321fd3dcc567bd8acb1eb1c270890a2
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2020-01-13 22:58:46 +0100

    Add implementation of BFS-Graph

--- src/main/java/algorithms/graphs/BFS/Graph.java
+++ src/main/java/algorithms/graphs/BFS/Graph.java
@@ -6,51 +6,33 @@ class Graph
 {
     private int V;                     // No. of vertices
     private LinkedList<Integer> adj[]; // Adjacency Lists
+    private Queue<Integer> que = new LinkedList<Integer>();
+    private Set<Integer> visited = new HashSet<Integer>();
 
     Graph(int v) {
         V = v;
         adj = new LinkedList[v];
-        for (int i = 0; i < v; ++i)
+        for (int i = 0; i < v; ++i) {
             adj[i] = new LinkedList();
+        }
     }
 
     void addEdge(int v, int w) {
         adj[v].add(w);
     }
 
-    // BFS traversal from a given source s
     void BFS(int s) {
-        // Mark all the vertices as not visited (false)
-        boolean visited[] = new boolean[V];
-
-        // Create a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<Integer>();
-
-        // Mark the current node as visited and enqueue it
-        visited[s] = true;
-        System.out.println("Starting at " + s);
-        queue.add(s);
-
-        while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it
-            s = queue.poll();
-            System.out.println("De-queueing " + s);
-//            System.out.print(s + " ");
-
-            // Get all adjacent vertices of the de-queued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            Iterator<Integer> i = adj[s].listIterator();
-            while (i.hasNext()) {
-                int n = i.next();
-                if (!visited[n]) {
-                    visited[n] = true;
-                    System.out.println("Queueing " + n);
-                    queue.add(n);
-                }
+        System.out.println(s);
+        visited.add(s);
+        for(int i=0; i < adj[s].size(); i++) {
+            Integer vertice = adj[s].get(i);
+            if(!visited.contains(vertice)) {
+                que.add(vertice);
+                visited.add(vertice);
             }
         }
+
+        if(!que.isEmpty())
+            BFS(que.poll());
     }
 }
-
-// based on https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
--- src/main/java/algorithms/graphs/DFS/Graph.java
+++ src/main/java/algorithms/graphs/DFS/Graph.java
@@ -1,8 +1,6 @@
 package algorithms.graphs.DFS;
 
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Stack;
 
 @SuppressWarnings("unchecked")
 class Graph
@@ -18,33 +16,11 @@ class Graph
     }
 
     void addEdge(int v, int w) {
-        adj[v].add(w);
+
     }
 
     void DFS(int v)
     {
-        boolean visited[] = new boolean[V];
 
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.add(v);
-
-        visited[v] = true;
-
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
-            System.out.print(current + " ");
-
-            Iterator<Integer> i = adj[current].listIterator();
-            while (i.hasNext())
-            {
-                int n = i.next();
-                if (!visited[n]) {
-                    stack.add(n);
-                    visited[n] = true;
-                }
-            }
-        }
     }
-
-    // based on https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/
 }
