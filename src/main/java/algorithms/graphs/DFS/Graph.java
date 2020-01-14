94f12566b44225ec6ad22604744a995a8c83c478
Patrick Nestmeyer <Patrick.Nestmeyer@gmail.com>
2020-01-14 20:11:12 +0100

    Add implementation of DFS-Graph

--- src/main/java/algorithms/graphs/DFS/Graph.java
+++ src/main/java/algorithms/graphs/DFS/Graph.java
@@ -1,12 +1,14 @@
 package algorithms.graphs.DFS;
 
-import java.util.LinkedList;
+import java.util.*;
 
 @SuppressWarnings("unchecked")
 class Graph
 {
     private int V;             // No. of vertices
-    private LinkedList[] adj;  // Adjacency List
+    private LinkedList<Integer>[] adj;  // Adjacency List
+    private Stack<Integer> stack = new Stack<Integer>();
+    private Set<Integer> visited = new HashSet<Integer>();
 
     Graph(int v) {
         V = v;
@@ -16,11 +18,23 @@ class Graph
     }
 
     void addEdge(int v, int w) {
+        adj[v].add(w);
 
     }
 
     void DFS(int v)
     {
+        System.out.println(v);
+        visited.add(v);
+        for(int i=0; i < adj[v].size(); i++) {
+            Integer vertice = adj[v].get(i);
+            if(!visited.contains(vertice)) {
+                stack.add(vertice);
+                visited.add(vertice);
+            }
+        }
 
+        if(!stack.isEmpty())
+            DFS(stack.pop());
     }
 }
